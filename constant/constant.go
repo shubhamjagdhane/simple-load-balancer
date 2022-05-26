14a2eea4b7a496bb9a0392bfdd18336064c08cf0
Shubham Jagdhane <shubhamjagdhane@ayoconnect.id>
2022-05-26 23:15:12 +0530

    added constants

---
+++ constant/constant.go
@@ -0,0 +1,13 @@
+package constant
+
+const (
+	HeaderContentType       = "Content-Type"
+	HeaderAccept            = "Accept"
+	MIMEApplicationJson     = "application/json"
+	Success                 = "SUCCESS"
+	Message_ok              = "ok"
+	DefaultTimeoutInSeconds = 3
+	SuccessfulResponse      = " successful"
+	Stage                   = "sandbox"
+	Dev                     = "development"
+)
